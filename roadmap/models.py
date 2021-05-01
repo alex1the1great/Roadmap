c30f7eee1c9a27085c892f8389d57be23c1c75d6
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-01 17:51:45 +0545

    create[roadmap]: goal model

--- roadmap/models.py
+++ roadmap/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+from django.core.exceptions import ValidationError
+
+User = settings.AUTH_USER_MODEL
+
+
+def validate_deadline_not_in_past(value):
+    current = datetime.now()
+    if value <= current:
+        raise ValidationError('Deadline should be in future date.')
+
+
+class Goal(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique_for_date='created')
+    deadline = models.DateTimeField(validators=[validate_deadline_not_in_past])
+    created = models.DateTimeField(auto_now_add=True)
+
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='goals')
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.name
+
