dbd8342e0ca14ee40fc119020c2f5b4e8ca0315d
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-02 09:11:56 +0545

    refactor: change datetime field to date field

--- roadmap/models.py
+++ roadmap/models.py
@@ -15,7 +15,7 @@ def validate_deadline_not_in_past(value):
 class Goal(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique_for_date='created')
-    deadline = models.DateTimeField(validators=[validate_deadline_not_in_past])
+    deadline = models.DateField(validators=[validate_deadline_not_in_past])
     created = models.DateTimeField(auto_now_add=True)
 
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='goals')
