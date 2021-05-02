ebaeaf2f77e19e74cb8062cee5a5fcc8a0829fb9
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-02 11:07:33 +0545

    feat: user can add new goal

--- roadmap/urls.py
+++ roadmap/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.goal_list, name='goal_list')
+    path('', views.goal_list, name='goal_list'),
+    path('/add/', views.goal_add, name='goal_add')
 ]
--- roadmap/views.py
+++ roadmap/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Goal
+from .forms import GoalForm
 
 
 def goal_list(request):
     goals = Goal.objects.all()
     return render(request, 'roadmap/goal_list.html', {'goals': goals})
+
+
+def goal_add(request):
+    form = GoalForm()
+    if request.method == 'POST':
+        form = GoalForm(request.POST)
+        if form.is_valid():
+            goal = form.save(commit=False)
+            goal.creator = request.user
+            goal.save()
+            return redirect('goal_list')
+    return render(request, 'roadmap/goal_add.html', {'form': form})
