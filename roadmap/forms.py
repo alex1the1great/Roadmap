32cd765f44425cbacba0dbb70258fa2237dedfbd
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-02 10:55:27 +0545

    create custom dateinput for deadline field

--- roadmap/forms.py
+++ roadmap/forms.py
@@ -3,8 +3,15 @@ from django import forms
 from .models import Goal
 
 
+class CustomDateInput(forms.DateInput):
+    input_type = 'date'
+
+
 class GoalForm(forms.ModelForm):
     class Meta:
         model = Goal
         fields = ['name', 'deadline']
+        widgets = {
+            'deadline': CustomDateInput()
+        }
 
