8f1e70c542016ef65a5e1a6070f57291315493c1
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-03 11:04:15 +0545

    feat: authenticated user can't visit login or signup route

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.views import LoginView
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
 
 from .forms import CustomUserCreationForm
 
@@ -10,7 +11,12 @@ class SignupView(CreateView):
     template_name = 'accounts/signup.html'
     success_url = reverse_lazy('login')
 
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('goal_list')
+
 
 class CustomLoginView(LoginView):
     template_name = 'accounts/login.html'
+    redirect_authenticated_user = 'goal_list'
 
