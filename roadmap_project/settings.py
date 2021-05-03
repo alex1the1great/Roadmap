bc7f98171942040a6211df927fd633f1d8a901b3
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-03 17:19:22 +0545

    add allowed host

--- roadmap_project/settings.py
+++ roadmap_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env.str('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['your-roadmap.herokuapp.com']
 
 # Application definition
 
