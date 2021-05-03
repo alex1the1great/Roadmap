880fcd97e7b4d25e22f21df706705b1be4598fdf
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-03 10:44:22 +0545

    feat: new user can be register

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
+    path('signup/', views.SignupView.as_view(), name='signup'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,14 @@
 from django.contrib.auth.views import LoginView
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+
+from .forms import CustomUserCreationForm
+
+
+class SignupView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'accounts/signup.html'
+    success_url = reverse_lazy('login')
 
 
 class CustomLoginView(LoginView):
